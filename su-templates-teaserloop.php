40e50cc937b81a0c5ea589f49907fc715bffae4c
MartinBurns <martinburnssv@gmail.com>
2014-12-11 17:30:34 +0100

    Improved formatting
    Moving to an unordered list

--- su-templates-teaserloop.php
+++ su-templates-teaserloop.php
@@ -1,4 +1,4 @@
-<div class="su-posts su-posts-teaser-loop">
+<ul class="su-posts su-posts-teaser-loop">
 	<?php
 		// Posts are found
 		if ( $posts->have_posts() ) {
@@ -6,12 +6,12 @@
 				$posts->the_post();
 				global $post;
 				?>
-				<div id="su-post-<?php the_ID(); ?>" class="su-post">
+				<li id="su-post-<?php the_ID(); ?>" class="su-post">
 					<?php if ( has_post_thumbnail() ) : ?>
 						<a class="su-post-thumbnail" href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
 					<?php endif; ?>
-					<h2 class="su-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-				</div>
+					<span class="su-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></span>
+				</li>
 				<?php
 			endwhile;
 		}
@@ -20,4 +20,4 @@
 			echo '<h4>' . __( 'Posts not found', 'su' ) . '</h4>';
 		}
 	?>
-</div>
+</ul>
